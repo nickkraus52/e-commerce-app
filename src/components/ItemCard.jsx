ce28fd9b0dc8c4c29b57299d8bf6b0181921e23d
nickkraus52 <51466360+nickkraus52@users.noreply.github.com>
2026-05-04 14:42:09 -0400

    Trash Icon on Item Cards

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -6,6 +6,7 @@ function ItemCard({ item }) {
     const URL = "http://localhost:5000/cart"
     const atMin = added <= 0
     const atMax = !item.stock.available || added >= item.stock.quantity
+    const trashIcon = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16"><path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/><path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/></svg>
 
     useEffect(() => {
         fetch(URL)
@@ -90,7 +91,7 @@ function ItemCard({ item }) {
                 {(item.stock.available) ? "" : (<p className="az-sold-out d-flex justify-content-center align-items-center mb-1">Sold Out!</p>)}
                 <span className="mt-auto d-flex justify-content-center align-items-center pt-1">
                     <div className="az-counter-wrap">
-                        <button onClick={() => handleCount("-")} className={atMin ? "az-counter-btn az-counter-btn-limit" : "az-counter-btn"}>-</button>
+                        <button onClick={() => handleCount("-")} className={atMin ? "az-counter-btn az-counter-btn-limit" : "az-counter-btn"}>{added == 1 ? (trashIcon) : "-"}</button>
                         <p className="az-counter-value">{added}</p>
                         <button onClick={() => handleCount("+")} className={atMax ? "az-counter-btn az-counter-btn-limit" : "az-counter-btn"}>+</button>
                     </div>
